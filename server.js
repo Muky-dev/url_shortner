a7b022ff4e6cc67bd761390d3865f552ce9c00aa
Muky-dev <69209901+Muky-dev@users.noreply.github.com>
2021-05-06 19:01:58 -0300

    Fixed server file

--- server.js
+++ server.js
@@ -6,10 +6,25 @@ const app = express();
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+// App "configs"
 app.use(cors());
-
+app.use(express.json());
+app.use(express.urlencoded({ limit: "10mb", extended: false }));
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', error => console.log(error));
+db.once('open', () => {
+    console.log("Database connected successfully");
+});
+
+// First route;
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
@@ -18,6 +33,8 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.use()
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
