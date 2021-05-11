09165667277c3434edd251c5688ad9db08656abb
Muky-dev <69209901+Muky-dev@users.noreply.github.com>
2021-05-11 19:15:30 -0300

    FreeCodeCamp Adjusts

--- controller/controller.js
+++ controller/controller.js
@@ -5,31 +5,32 @@ import uniqueHash from './hasher.js'
 
 export async function postUrl(req, res) {
     const url = req.body.url;
-    const hashed = uniqueHash(url.hashCode());
     //const test_reg = /^http(s?):\/\/(www\.\w+|\w+)\.\w{1,5}/i
     const replace_reg = /^http(s?):\/\//i
     const url_replaced = url.replace(replace_reg, '');
     if (!replace_reg.test(url)) {
-        res.status(500).json({ error: 'invalid url' });
+        res.json({ error: 'invalid url' });
     } else {
         try {
             const ipAddress = await lookup(url_replaced);
+            const hashed = uniqueHash(ipAddress.hashCode());
+            console.log(hashed);
             const modeled = {
                 original_url: url,
                 short_url: hashed
             }
             try {
                 await new Url(modeled).save();
-                res.status(200).json(modeled);
+                res.json(modeled);
             } catch (error) {
                 if (error.code === 11000) {
-                    res.status(200).json(modeled);
+                    res.json(modeled);
                 } else {
-                    res.status(500).json(error);
+                    res.json(error);
                 }
             }
         } catch {
-            res.status(500).json({"error":"invalid url" });
+            res.json({"error":"invalid url" });
         }
     }
 }
@@ -52,6 +53,6 @@ export async function getUrl(req, res) {
         const url = await Url.findOne({ short_url: shortUrl });
         res.redirect(url.original_url);
     } catch (error) {
-        res.status(500).json({ message: "Inexistent URL" });
+        res.json({ message: "Inexistent URL" });
     }
 }
